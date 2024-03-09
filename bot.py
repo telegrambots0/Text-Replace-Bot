d30a437bdde7166c6b3c1ae12639d81469c48b93
telegrambots0 <146921432+telegrambots0@users.noreply.github.com>
2024-03-09 18:31:09 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-# the logging things
+# Logging configuration
 import logging
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -7,8 +7,7 @@ logger = logging.getLogger(__name__)
 import os
 from pyromod import listen
 
-
-# the secret configuration specific things
+# Secret configuration specific things
 if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
 else:
@@ -17,19 +16,17 @@ else:
 import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-
 DOWNLOAD_LOCATION = "/downloads"
 
-if __name__ == "__main__" :
-    
-    plugins  = dict(
+if __name__ == "__main__":
+    plugins = dict(
         root="plugins"
     )
     app = pyrogram.Client(
         ":memory:",
         bot_token=Config.BOT_TOKEN,
         api_id=Config.APP_ID,
-        api_hash=Config.API_HASH,        
+        api_hash=Config.API_HASH,
         plugins=plugins,
         parse_mode="html"
     )
