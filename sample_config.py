fadccfbd1ef3c0821bff6e400a012f2736eba836
telegrambots0 <146921432+telegrambots0@users.noreply.github.com>
2024-03-09 17:20:58 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -4,14 +4,14 @@ class Config(object):
     
     DOWNLOAD_LOCATION = "./DOWNLOADS"
 
-    BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6796993711:AAE_xk1idxA_FbU5mcXK5_YaZdCkI6p9PaE")
 
-    APP_ID = int(os.environ.get("APP_ID", 12345))
+    APP_ID = int(os.environ.get("APP_ID", "27815405"))
 
-    API_HASH = os.environ.get("API_HASH", "")    
+    API_HASH = os.environ.get("API_HASH", "4e70821cd2af3322f7cf2f2887e32821")    
     
-    CAPTION_TEXT = os.environ.get("CAPTION_TEXT", "")
+    CAPTION_TEXT = os.environ.get("CAPTION_TEXT", "Swarg Ronny")
 
     CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "bottom")
 
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "27815405").split())
